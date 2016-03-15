4f54de7f8258042f4f3331261f6cefe473bac656
Juan Manuel Rodulfo <juanmanuel.rodulfo@mail.rakuten.com>
2016-03-15 18:48:58 +0100

    added require 'rest-client'

--- lib/api_connector.rb
+++ lib/api_connector.rb
@@ -2,6 +2,7 @@ require "api_connector/version"
 require 'api_connector/api_connector'
 require 'active_support/core_ext/hash/keys'
 require 'logger'
+require 'rest-client'
 
 module ApiConnector
   def self.new(options={})
